3ff001f92238d939f5e662db4d95da90e6bafbdb
Mishunin <102225629+Mishunin@users.noreply.github.com>
2022-06-08 14:15:27 +0700

    change

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+const popup = document.querySelector('.popup')
+
+let popupToggle = document.querySelector('.profile__edit-button');
+let popupClose = document.querySelector('.popup__closed');
+let popupName = document.querySelector('.popup__input_name'); 
+let popupProfession = document.querySelector('.popup__input_profession');
+let popupTitle = document.querySelector('.profile__name');   
+let popupSubtitle = document.querySelector('.profile__text'); 
+
+function openPopup() { 
+    popup.classList.add('popup_opened'); 
+    popupName.value = popupTitle.textContent; 
+    popupProfession.value = popupSubtitle.textContent; 
+};
+ 
+
+function closePopup() { 
+    popup.classList.remove('popup_opened'); 
+};
+
+let popupForm = document.querySelector('.popup__form'); 
+
+function formSubmitHandler (evt) { 
+evt.preventDefault();  
+popupTitle.textContent = popupName.value; 
+popupSubtitle.textContent = popupProfession.value; 
+closePopup();
+};
+
+popupForm.addEventListener('submit', formSubmitHandler); 
+popupClose.addEventListener('click', closePopup); 
+popupToggle.addEventListener('click', openPopup); 
